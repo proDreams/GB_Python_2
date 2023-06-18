0d2e1af252b3deba6b43b88492aa5f58f0af1240
Ivan Ashikhmin <sushkoos@gmail.com>
2023-06-18 21:17:26 +0400

    Seminar 2 HW

--- Seminar 2/Seminar 2 HW/task_1.py
+++ Seminar 2/Seminar 2 HW/task_1.py
@@ -7,17 +7,9 @@ MAX_DIGIT = 9
 
 def dec_to_hex(num: int):
     result = []
-    alp = {10: 'A',
-           11: 'B',
-           12: 'C',
-           13: 'D',
-           14: 'E',
-           15: 'F'}
+    alp = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     while num > 0:
-        if num % CONST_HEX > MAX_DIGIT:
-            result.append(str(alp[num % CONST_HEX]))
-        else:
-            result.append(str(num % CONST_HEX))
+        result.append(str(alp[num % CONST_HEX]))
         num //= CONST_HEX
     return "".join(result[::-1])
 
