245c4527e2e94dc3b06ff095824dcbcd15e79a8a
Ivan Ashikhmin <sushkoos@gmail.com>
2023-06-25 22:14:14 +0400

    Seminar 4 HW

---
+++ Seminar 4/Seminar 4 HW/task_2.py
@@ -0,0 +1,13 @@
+# Напишите функцию для транспонирования матрицы
+def flip_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(i, len((matrix[i]))):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+predefined_matrix = [[1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9]]
+print(*predefined_matrix, sep="\n")
+flip_matrix(predefined_matrix)
+print()
+print(*predefined_matrix, sep="\n")
